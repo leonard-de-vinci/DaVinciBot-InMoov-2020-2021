d8300db1dba8763a7801a26c0f0f6c3fddddeaec
wimausberlin <poignon.wim@gmx.de>
2021-05-31 12:46:46 +0200

    update espros

--- espros - platformio/main.cpp
+++ espros - platformio/main.cpp
@@ -1,6 +1,6 @@
 /*
- * rosserial Publisher Example
- * Prints "hello world!"
+ * rosserial Subscriber through WiFi Example
+ * Move a servo after publishing a msg
  * This intend to connect to a Wifi Access Point
  * and a rosserial socket server.
  * You can launch the rosserial socket server with
@@ -8,30 +8,33 @@
  * The default port is 11411
  *
  */
-#include <Servo.h>
-#include <ESP8266WiFi.h>
+#include <Servo.h> //lib to control the servomotor for InMoov
+#include <ESP8266WiFi.h> //lib for the ESP8266
 #include <ros.h>
 #include <std_msgs/String.h> //allow to work with str msg with ros (eg. publish a string message)
 #include <std_msgs/Int16.h> //allow to work with int msg with ros (eg. move a servo with an int message)
 #include <string>
 
+// Define a servo motor object 
 Servo servo;
 
-const char* ssid = "PULV_DVIC";
-const char* password = "5UKAPIp!dvic";
-// Set the rosserial socket server IP address
-IPAddress server(172,21,72,140);
+// Caracteristic of the same wifi that is connected to the jetson
+const char* ssid = "PULV_DVIC"; 
+const char* password = "5UKAPIp!dvic"; 
+// Set the rosserial socket server IP address (must be )
+IPAddress server(172,21,72,156);
 // Set the rosserial socket server port
-const uint16_t serverPort = 11411;
+const uint16_t serverPort = 11412;
 
 #define message "servo moved"
-#define LED D0
-#define DEBUG 0
+#define LED D0 
+#define DEBUG 0 //if 1, you can have acces to the serial (monitor)
 int i;
 
-////  All subscriber messages callbacks here
+// All subscriber messages callbacks here
 void angleCallback(const std_msgs::Int16& msg) {
-  Serial.print("sucess");
+  if(DEBUG)
+  { Serial.print("sucess"); }
   servo.attach(D1);
   servo.write(abs(msg.data));
 }
@@ -40,10 +43,8 @@ ros::NodeHandle nh;
 // Make a chatter publisher
 std_msgs::String str_msg;
 //ros::Publisher chatter("chatter", &str_msg);
-ros::Subscriber<std_msgs::Int16> sub("led", &angleCallback);
+ros::Subscriber<std_msgs::Int16> sub("led", &angleCallback); //the name of the subscriber in brackets sub(_subscriberName_, _functionCall_)
 
-// Be polite and say hello
-char hello[13] = "hello world!";
 
 void setup()
 {
@@ -58,11 +59,13 @@ void setup()
 
   // Connect the ESP8266 the the wifi AP
   WiFi.begin(ssid, password);
-  if(DEBUG){
+  
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
-      Serial.print(".");
+      if(DEBUG)
+        {Serial.print(".");}
     }
+  if(DEBUG){
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
