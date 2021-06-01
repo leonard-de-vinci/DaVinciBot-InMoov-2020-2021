ad249fa176f3c3828dee1eb2e2ca09dbc954dc4c
wimausberlin <poignon.wim@gmx.de>
2021-06-01 17:21:29 +0200

    Json modifications

---
+++ inmoov_ws/src/api/src/json_generator.py
@@ -0,0 +1,64 @@
+import json
+import re
+
+class JSON_generator:
+    def __init__(self,path='inmoov_control.json'):
+        self.path = path
+        self.open_json()
+    
+    def open_json(self):
+        with open(self.path) as f:
+            self.control_json = json.load(f) 
+            
+        def flatten_json():
+            """
+                Flatten json object with nested keys into a single level.
+                Args:
+                    nested_json: A nested json object.
+                Returns:
+                    The flattened json object if successful, None otherwise.
+            """
+            out = {}
+
+            def flatten(x, name=''):
+                if type(x) is dict:
+                    for a in x:
+                        flatten(x[a], name + a + '/')
+                elif type(x) is list:
+                    i = 0
+                    for a in x:
+                        flatten(a, name + str(i) + '/')
+                        i += 1
+                else:
+                    out[name[:-1]] = x
+                    
+            flatten(self.control_json)
+            self.control_json = out            
+        flatten_json()
+        
+    def reset(self):
+        for key in self.control_json.keys():
+            self.control_json[key] = 0
+
+    def change_values(self, control_dict):
+        for key,value in control_dict.items():
+            self.control_json[key] = value
+            
+    def save_json(self):
+        temp_dict = {}
+        for key, value in self.control_json.items():
+            s = temp_dict
+            tokens = re.findall(r'\w+', key)
+            for count, (index, next_token) in enumerate(zip(tokens, tokens[1:] + [value]), 1):
+                value = next_token if count == len(tokens) else [] if next_token.isdigit() else {}
+                if isinstance(s, list):
+                    index = int(index)
+                    while index >= len(s):
+                        s.append(value)
+                elif index not in s:
+                    s[index] = value
+                s = s[index]
+        self.control_json = temp_dict
+                
+        with open(self.path, 'w') as outfile:
+            json.dump(self.control_json, outfile, indent=4)
---
+++ inmoov_ws/src/api/src/main.py
@@ -0,0 +1,183 @@
+from json_generator import JSON_generator
+
+import roslaunch
+
+import Tkinter as tk
+import ttk
+
+control_json = JSON_generator("../control/inmoov_control.json")
+control_json.reset()
+
+def open_hand():
+    control_dict = {
+        "robot/arm/left/hand/finger/middle/servo/angle": 100,
+        "robot/arm/left/hand/finger/ring/servo/angle": 50,
+        "robot/arm/left/hand/finger/pointer/servo/angle": 100,
+        "robot/arm/left/hand/finger/little/servo/angle": 100,
+        "robot/arm/left/hand/finger/thumb/servo/angle": 50,
+    }
+    control_json.change_values(control_dict)
+    control_json.save_json()
+
+def close_hand():
+    control_dict = {
+        "robot/arm/left/hand/finger/middle/servo/angle": 50,
+        "robot/arm/left/hand/finger/ring/servo/angle": 100,
+        "robot/arm/left/hand/finger/pointer/servo/angle": 50,
+        "robot/arm/left/hand/finger/little/servo/angle": 50,
+        "robot/arm/left/hand/finger/thumb/servo/angle": 100,
+    }
+    control_json.change_values(control_dict)
+    control_json.save_json()
+
+#write the new window function which
+#will be called when button pressed
+def demo_window():
+    window = tk.Toplevel(root)
+    canvas = tk.Canvas(window, height=HEIGHT, width=WIDTH)
+
+    window.geometry('600x600')
+    b1 = tk.Button(window, text ="Open hand", command = open_hand)
+    b2 = tk.Button(window, text ="Close hand", command = close_hand)
+
+    b1.pack()
+    b2.pack()
+    canvas.pack()
+
+def manual_window():
+    window = tk.Toplevel(root)
+    canvas = tk.Canvas(window, height=HEIGHT, width=WIDTH)
+
+    window.geometry('1000x100')
+    # Label
+    ttk.Label(window, text = "Thumb", 
+            font = ("Arial", 10)).grid(column = 0, 
+            row = 15, padx = 10, pady = 5)
+    
+    ttk.Label(window, text = "Index", 
+            font = ("Arial", 10)).grid(column = 1, 
+            row = 15, padx = 10, pady = 5)
+
+    ttk.Label(window, text = "Middle", 
+            font = ("Arial", 10)).grid(column = 2, 
+            row = 15, padx = 10, pady = 5)
+
+    ttk.Label(window, text = "Ring", 
+            font = ("Arial", 10)).grid(column = 3, 
+            row = 15, padx = 10, pady = 5)
+
+    ttk.Label(window, text = "Pinky", 
+            font = ("Arial", 10)).grid(column = 4, 
+            row = 15, padx = 10, pady = 5)
+
+    ttk.Label(window, text = "Mouth", 
+            font = ("Arial", 10)).grid(column = 5, 
+            row = 15, padx = 10, pady = 5)
+    
+    ttk.Label(window, text = "Eye right", 
+            font = ("Arial", 10)).grid(column = 6, 
+            row = 15, padx = 10, pady = 5)
+
+    ttk.Label(window, text = "Eye left", 
+            font = ("Arial", 10)).grid(column = 7, 
+            row = 15, padx = 10, pady = 5)
+
+    ttk.Label(window, text = "Neck translation", 
+            font = ("Arial", 10)).grid(column = 8, 
+            row = 15, padx = 10, pady = 5)
+
+    ttk.Label(window, text = "Neck rotation", 
+            font = ("Arial", 10)).grid(column = 9, 
+            row = 15, padx = 10, pady = 5)
+
+    ttk.Entry(window, width =10,textvariable="0",
+            font = ("Arial", 10)).grid(column = 0, 
+            row = 16, padx = 10, pady = 5)
+    ttk.Entry(window, width =10,textvariable="0",
+            font = ("Arial", 10)).grid(column = 1, 
+            row = 16, padx = 10, pady = 5 )
+    ttk.Entry(window, width =10,textvariable="0",
+            font = ("Arial", 10)).grid(column = 2, 
+            row = 16, padx = 10, pady = 5 )
+    ttk.Entry(window, width =10,textvariable="0",
+            font = ("Arial", 10)).grid(column = 3, 
+            row = 16, padx = 10, pady = 5 )
+    ttk.Entry(window, width =10,textvariable="0",
+            font = ("Arial", 10)).grid(column = 4, 
+            row = 16, padx = 10, pady = 5 )
+    ttk.Entry(window, width =10,textvariable="0",
+            font = ("Arial", 10)).grid(column = 5, 
+            row = 16, padx = 10, pady = 5)
+    ttk.Entry(window, width =10,textvariable="0",
+            font = ("Arial", 10)).grid(column = 6, 
+            row = 16, padx = 10, pady = 5 )
+    ttk.Entry(window, width =10,textvariable="0",
+            font = ("Arial", 10)).grid(column = 7, 
+            row = 16, padx = 10, pady = 5 )
+    ttk.Entry(window, width =10,textvariable="0",
+            font = ("Arial", 10)).grid(column = 8, 
+            row = 16, padx = 10, pady = 5 )
+    ttk.Entry(window, width =10,textvariable="0",
+            font = ("Arial", 10)).grid(column = 9, 
+            row = 16, padx = 10, pady = 5 )
+
+    ttk.Button(window, width =10, text="Valider").grid(column = 9, 
+            row = 16, padx = 10, pady = 5 )
+    canvas.pack()
+
+HEIGHT = 600
+WIDTH = 600
+#create original window (title not need but why not?)
+root = tk.Tk()
+root.title("new window making machine: ")
+canvas = tk.Canvas(root, height=100, width=300)
+canvas.pack()
+#create button that will be placed
+button1 = tk.Button(root, text="Demo",
+                   command=lambda: demo_window())
+button1.place(x=500, y=100)
+button2 = tk.Button(root, text="Manual",
+                   command=lambda: manual_window())
+button2.place(x=100, y=100)
+#can use .grid() or .place() instead of pack .place()
+#is the best according to me if you want the most control of positions
+button1.pack()
+button2.pack()
+root.mainloop()
+
+
+
+
+
+#def __main__():
+    
+"""
+    control_json = JSON_generator("../control/inmoov_control.json")
+
+    # Creating tkinter window
+    window = tk.Tk()
+    window.geometry('600x600')
+    # Label
+    ttk.Label(window, text = "Select the Month :", 
+            font = ("Arial", 10)).grid(column = 0, 
+            row = 15, padx = 10, pady = 25)
+
+    n = tk.StringVar()
+    monthchoosen = ttk.Combobox(window, width = 27,state="readonly", 
+                                textvariable = n)
+
+    # Adding combobox drop down list
+    monthchoosen['values'] = tuple("Thumb",
+                                   "Index",
+                                   "Middle",
+                                   "Ring",
+                                   "Pinky")
+
+    monthchoosen.grid(column = 1, row = 15)
+
+    # Shows february as a default value
+    monthchoosen.current(1)
+    window.mainloop()"""
+#__main__()
+
+
